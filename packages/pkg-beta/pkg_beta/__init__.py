7d49e3ccec1a2f770acdf02ef341387f7592af87
Tyler Payne <tylerpayne@microsoft.com>
2026-02-13 15:48:57 -0500

    test4: change beta middle node

--- packages/pkg-beta/pkg_beta/__init__.py
+++ packages/pkg-beta/pkg_beta/__init__.py
@@ -8,3 +8,8 @@ __version__ = "0.1.0"
 def greet() -> str:
     """Return a greeting from beta, including alpha's greeting."""
     return f"Hello from beta! Also, {alpha_greet()}"
+
+
+def test4_beta_change() -> str:
+    """Test 4: change beta middle node."""
+    return "beta changed - beta and gamma should rebuild"
